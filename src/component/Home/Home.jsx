a070a12f3a8d6665dedbd7c3cce6357a2a374b84
lokesh <21be0158@sipnaengg.ac.in>
2025-04-18 19:40:26 +0530

    Changes is done

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -54,7 +54,7 @@ function Home() {
       const res = await fetch("https://githubreviewerbackend-2.onrender.com/api/review", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({  username: "Lokeshwagh"}),
+        body: JSON.stringify({  username}),
       });
 
       const data = await res.json();
